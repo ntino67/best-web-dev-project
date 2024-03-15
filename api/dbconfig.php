15a0fa37f5c2acc16037f5c790c5c5135a41c858
kellian <kellian.bechtel@hotmail.com>
2024-03-15 22:20:49 +0100

    Added connectionString file

--- api/dbconfig.php
+++ api/dbconfig.php
@@ -1,16 +1,19 @@
 <?php
 global $conn;
-$servername = "localhost";
-$username = "root";
-$password = "kellian";
-$databasename = "web_project";
+
+$connectionString = file_get_contents("connectionstring.json");
+
+$connectionString = json_decode($connectionString);
+
+$servername = $connectionString->servername;
+$username = $connectionString->username;
+$password = $connectionString->password;
+$databasename = $connectionString->databasename;
 
 try {
      $conn = new PDO("mysql:host=$servername;dbname=$databasename;charset=utf8mb4", $username, $password);
 }
 catch (Exception $e) {
-  $message = $e->getMessage;
+  $message = $e->getMessage();
   echo "DB connection failed $message";
 }
-
-?>
