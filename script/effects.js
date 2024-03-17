1d4904f4fc4c2f59418d570a8ba50534336a92d8
ntino67 <matteo.heidelberger@viacesi.fr>
2024-03-17 23:09:42 +0100

    Redesign layout and enhance user interactivity
    This redesign centers content and improves UX with hover effects and a confirmation prompt for logout. Also now provides mobile support with a responsive navigation bar.

--- script/effects.js
+++ script/effects.js
@@ -1,5 +1,22 @@
-$('.line-effect').on('mouseenter', function () {
-    $(this).addClass('hover');
-}).on('mouseleave', function () {
-    $(this).removeClass('hover');
+$(document).ready(function () {
+    $('.line-effect').on('mouseenter', function () {
+        $(this).addClass('hover');
+    }).on('mouseleave', function () {
+        $(this).removeClass('hover');
+    });
+
+    $(".menu-burger").click(function () {
+        $(".nav-links").toggleClass("mobile-menu");
+    });
+
+    $('#logout').click(function (e) {
+        e.preventDefault();  // Prevent the default action (redirect to logout)
+
+        var result = confirm("Are you sure you want to logout?");
+
+        if (result) {
+            // If confirmed, redirect to login.html
+            window.location.href = "login.html";
+        }
+    });
 });
