8c59fe78f858d5ef89289dd89c84dbe7c3c40c07
ntino67 <matteo.heidelberger@viacesi.fr>
2024-04-03 16:38:00 +0200

    Refactor dropdown filter functionality
    Introduced changes to the dropdown filter functionality in filter-fetch.js. This includes extracting the actual text of the dropdown value to display as the filter tag, instead of the code, and improving the data handling when fetching the cities via AJAX call. Also adjusted the way re-enabling of disabled options is being handled.

--- script/filter-fetch.js
+++ script/filter-fetch.js
@@ -1,9 +1,10 @@
 function handleDropdownChange(elementId, containerClass, defaultValue = "0") {
     $(elementId).change(function () {
         const dropdownValue = $(this).val();
+        const dropdownText = $(this).find('option:selected').text();
         if (dropdownValue !== defaultValue) {
             const filterContainer = $(`.filter-container.${containerClass}`);
-            const newFilter = `<div class="filter-tag" data-origin="${elementId}">${dropdownValue}</div>`;
+            const newFilter = `<div class="filter-tag" data-origin="${elementId}" data-value="${dropdownValue}">${dropdownText}</div>`;
             filterContainer.append(newFilter);
             filterContainer.show();
             $(`${elementId} option[value='${dropdownValue}']`).prop('disabled', true);
@@ -16,7 +17,8 @@ $(document).ready(function () {
     $.ajax({
         url: 'http://webp.local/api/city',
         type: 'GET',
-        success: function (cities) {
+        success: function (response) {
+            var cities = response.data; //access the data property of the response
             $.each(cities, function (index, city) {
                 $("#cities")
                     .append($("<option></option>")
@@ -57,8 +59,8 @@ $(document).ready(function () {
     $(document).on('click', '.filter-tag', function () {
         const $this = $(this);
         const originId = $this.data('origin');
-        const filterValue = $this.text();
-        $(`${originId} option[value='${filterValue}']`).prop('disabled', false);
+        const filterValue = $this.data('value'); // Get the stored value
+        $(`${originId} option[value='${filterValue}']`).prop('disabled', false); // enable the option with the stored value
         $this.remove();
         const $parentContainer = $this.parent();
         if (!$parentContainer.children().length) {
