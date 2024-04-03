8147228c275ffe323ed3ed2646a0a72f0fdf6598
ntino67 <matteo.heidelberger@viacesi.fr>
2024-04-04 01:48:52 +0200

    Implement search functionality in internship page but fuck it isn't implemented for internships yet :(
    Refactored the loadInternships function in the script file, separating the URL into a variable for clearer code structure. A keypress function was added for the search bar to detect when the 'Enter' key is pressed. The updated functionality includes reloading the internships page with a filter based on the input if it is non-empty. If the input is empty, it will reload all internships.

--- script/load-internship.js
+++ script/load-internship.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
-    // Get internship offers using AJAX GET request
-    loadInternships('http://webp.local/api/internship', '#internship-container', function (newElement, offer) {
+    // Load internship offers initially using AJAX GET request
+    var url = 'http://webp.local/api/internship';
+    loadInternships(url, '#internship-container', function (newElement, offer) {
         // Insert bottom info
         let slots = offer.available_slots;
         $(".search-result-bottom-info-element", newElement).html(slots + (slots === 1 ? " Slot" : " Slots"));
@@ -9,5 +10,31 @@ $(document).ready(function () {
         // Insert right info
         $(".info-text", newElement).eq(0).html("Base Salary: " + offer.base_salary + "$");
         $(".info-text", newElement).eq(1).html(offer.company_name);
+    });
+
+    $('.search-bar-small').keypress(function (e) {
+        console.log('Key Pressed:', e.which);
+        // When enter key is pressed
+        if (e.which == 13) {  // 13 is the enter key's keycode
+            let input = $(this).val();
+
+            if (input) {
+                // Load internships with filter if there's input
+                console.log(input)
+                url = `http://webp.local/api/internship?orderby=id_internship_offer&filter=internship_offer_title startswith ${input}`;
+            } else {
+                // Load all internships if input is empty
+                url = 'http://webp.local/api/internship';
+            }
+
+            $('#internship-container').empty();
+
+            loadInternships(url, '#internship-container', function (newElement, offer) {
+                // specific handler code
+            });
+
+            // prevent the default action
+            return false;
+        }
     });
 });
