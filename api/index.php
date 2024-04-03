f681c1058af163f3dd1701037fca0fc4d5f0c632
kellianb <kellian.bechtel@hotmail.com>
2024-04-03 17:45:38 +0200

    Added Authentication - Added JWT Authentication

--- api/index.php
+++ api/index.php
@@ -7,8 +7,14 @@ spl_autoload_register(function($class) {
   require __DIR__ . "/src/$class.php";  
 });
 
+// Load php dependencies
+require_once(__DIR__ . "/vendor/autoload.php");
+
 // Set content type to json and define charset
 header('Content-Type: application/json; charset=utf-8');
+
+// Allow CORS
+header("Access-Control-Allow-Origin: *");
 
 // Set Error and Exception handlers
 set_error_handler("ErrorHandler::handleError");
@@ -27,6 +33,22 @@ while (true) {
   if (array_shift($requestURI) == "api") {
     break;
   }
+}
+
+$controller = new AuthController;
+
+// Auth endpoint
+if ($requestURI[0] == "auth")
+{
+  $controller->authenticateUser($_SERVER['REQUEST_METHOD']);
+
+  exit();
+}
+else if (!array_key_exists("HTTP_AUTHORIZATION_TOKEN", $_SERVER)|| !$controller->verifyToken($_SERVER["HTTP_AUTHORIZATION_TOKEN"]))
+{
+  http_response_code(401);
+  echo json_encode(["message" => "Unauthorized"]);
+  exit();
 }
 
 // Process request type, send to corrent API endpoint 
