c8f842c20cf5a386cf0e44f960a756d3072d38f7
kellianb <kellian.bechtel@hotmail.com>
2024-04-04 17:14:05 +0200

    Moved RelatedClassModel to inheriting from Model

--- api/src/RelatedClassModel.php
+++ api/src/RelatedClassModel.php
@@ -1,90 +1,47 @@
 <?php
 
 // Get the classes a user is related to
-class RelatedClassModel {
+class RelatedClassModel extends Model{
 
   private PDO $conn;
 
   public function __construct()
   {
-    $this->conn = Database::getConnection();
+      parent::__construct();
+
+      $this->insert_params = ["id_class"];
+
+      $this->use_paging = false;
+
+      $this->use_parent_id = true;
+
+      $this->sql_getAll = "
+      SELECT C.id_class as id_class, class_year as class_name
+      FROM Related_to_class
+              JOIN web_project.Classes C on C.id_class = Related_to_class.id_class
+      WHERE id_user = :id_parent
+      ";
+
+      $this->sql_get = "
+      SELECT C.id_class as id_class, class_year as class_name
+      FROM Related_to_class
+              JOIN web_project.Classes C on C.id_class = Related_to_class.id_class
+      WHERE id_user = :id_parent
+        AND C.id_class = :id_object
+      "; 
+
+      $this->sql_create = "
+      INSERT INTO Related_to_class (id_user,
+                                  id_class)
+      VALUES (:id_parent,
+              :id_class)
+      ";
+
+      $this->sql_delete = "
+      DELETE
+      FROM Related_to_class
+      WHERE id_user = :id_parent
+        AND id_class = :id_object
+      ";
   }
- 
-  // @return mixed[]
-  public function getAll(string $id_user): array {
-    $sql = "
-    SELECT C.id_class as id_class, class_year as class_name
-    FROM Related_to_class
-            JOIN web_project.Classes C on C.id_class = Related_to_class.id_class
-    WHERE id_user = :id_user
-    ";
-
-    $statement = $this->conn->prepare($sql);
-
-    $statement->bindValue(":id_user", $id_user);
-
-    $statement->execute();
-
-    return $statement->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  // @return mixed[]
-  public function get(string $id_user, string $id_class) : array | false
-  {
-    $sql = "
-    SELECT C.id_class as id_class, class_year as class_name
-    FROM Related_to_class
-            JOIN web_project.Classes C on C.id_class = Related_to_class.id_class
-    WHERE id_user = :id_user
-      AND C.id_class = :id_class
-    ";
-
-    $statement = $this->conn->prepare($sql);
-
-    $statement->bindValue(":id_user", $id_user);
-    $statement->bindValue(":id_class", $id_class);
-
-    $statement->execute();
-
-    return $statement->fetch(PDO::FETCH_ASSOC);
-  }
-
-  // @return mixed[] $data
-  public function create(string $id_user, string $id_class) : void
-  {
-    $sql = "
-    INSERT INTO Related_to_class (id_user,
-                                id_class)
-    VALUES (:id_user,
-            :id_class)
-    ";
-
-    $statement = $this->conn->prepare($sql);
-
-    $statement->bindValue(":id_user", $id_user);
-    $statement->bindValue(":id_class", $id_class);
-
-    $statement->execute();
-
-    return;
-  }
-
-  public function delete(string $id_user, string $id_class) : int
-  {
-    $sql = " 
-    DELETE
-    FROM Related_to_class
-    WHERE id_user = :id_user
-      and id_class = :id_class
-    ";
-
-  $statement = $this->conn->prepare($sql);
-
-  $statement->bindValue(":id_user", $id_user);
-  $statement->bindValue(":id_class", $id_class);
-
-  $statement->execute();
-
-  return $statement->rowCount();
-  }
-}
+} 
